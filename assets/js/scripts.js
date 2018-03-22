a3b4dcf6c3a779855a0719e6c62a79299c0e5da8
kmsemo1 <32209919+kmsemo1@users.noreply.github.com>
2018-03-21 18:09:30 -0700

    fix back before https commit

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     // (function () {
-    //     var myLatlng = new google.maps.LatLng(117.1611, 32.7157);
+    //     var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
 
     //         var styles = [
     //             {
